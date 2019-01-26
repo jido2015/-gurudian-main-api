f1f0ad00febc821a9bdbd9b35048625777fab2b6
Bashorun Olajide <bashorun.olajide@gmail.com>
2019-01-26 09:31:24 +0100

    Set application controller to authenticate api request before allowing them to go through

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  include Response
+  include ExceptionHandler
+
+  before_action :authenticate_request
+  attr_reader :current_user
+
+  private
+
+  # Check for valid request token and return user
+
+  def authenticate_request
+    @current_user = AuthenticateUserService.new(request.headers).call
+
+  end
 end
