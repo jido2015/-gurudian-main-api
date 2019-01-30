7aa3188f0fa9d86639285ba7f3e5dfdb99e61607
Bashorun Olajide <bashorun.olajide@gmail.com>
2019-01-30 14:48:57 +0100

    Add home controller with just ping method with no content

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'home/ping'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
